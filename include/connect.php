b08443afc80eb6bd9b8d32b0a8b320cc870cf9c7
Sylvia Duarte <sylviaelizabethduarte@gmail.com>
2022-06-08 10:34:23 -0500

    Connect period data DB

--- include/connect.php
+++ include/connect.php
@@ -2,7 +2,7 @@
 
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'personal_site');
+define('DB_DATABASE', 'period_tracker');
 define('DB_HOSTNAME', 'localhost');
 define('DB_PORT', 3306);
 
